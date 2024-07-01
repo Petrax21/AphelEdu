6940d7b39df8c975038d60c477fa098bd87be518
Muhammed Mustafa Şitil <dyk.593.m.mustafa.sitil@gmail.com>
2024-07-01 14:18:26 +0300

    Projeler eklendi

---
+++ 01-hello-world/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Merhaba" << std::endl;
+
+    return 0;
+}
---
+++ 02-cin/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Adınız nedir? : ";
+
+    std::string name;
+
+    std::cin >> name;
+
+    std::cout << "Merhaba " << name << std::endl;
+
+    return 0;
+}
